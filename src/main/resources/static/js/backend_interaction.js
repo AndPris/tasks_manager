e9a3de7a3fd9c68daf0fc05c8b19efc6aec8c2b8
Andrii <andpris527@gmail.com>
2024-08-29 15:50:56 +0300

    Begin refactor GET

--- src/main/resources/static/js/backend_interaction.js
+++ src/main/resources/static/js/backend_interaction.js
@@ -1,7 +1,15 @@
 import {validateForm} from "data_validation.js";
+import {displayTasks} from "dom_interaction.js";
 
 const baseURL = "/api/tasks";
 const defaultNetworkErrorMessage = "Network response was not ok";
+const pageSize = 5;
+let pageNumber = 0;
+let sortOrders= [];
+let descriptionToFind = '';
+let prioritySortOrder = 0;
+let finishDateSortOrder = 0;
+
 
 //POST
 export async function addTaskToDB(event) {
@@ -141,4 +149,134 @@ async function putTask(taskId, data) {
 
     if (!response.ok)
         throw new Error(defaultNetworkErrorMessage);
+}
+
+
+//GET
+export async function loadTasks(queryString) {
+    queryString = queryString ? queryString : getDefaultQueryString();
+
+    try {
+        let [tasks, pageInfo] = await getTasks(queryString);
+        displayTasks(tasks);
+        updatePaginationButtons(pageInfo);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function getDefaultQueryString() {
+    let queryString = `?page=${pageNumber}&size=${pageSize}&sort=done,asc`;
+
+    for(let sortOrder of sortOrders) {
+        if(sortOrder[1] === 0)
+            continue;
+
+        queryString += `&sort=${sortOrder[0]},${getSortingOrderByCode(sortOrder[1])}`;
+    }
+
+    return queryString;
+}
+
+function getSortingOrderByCode(code) {
+    if (code === 1)
+        return "asc";
+    if (code === 2)
+        return "desc";
+}
+
+async function getTasks(queryString) {
+    const response = await fetch(`${baseURL}${queryString}`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (!response.ok)
+        throw new Error(defaultNetworkErrorMessage);
+
+    const data = await response.json();
+    return [data._embedded.tasks, data.page];
+}
+
+function updatePaginationButtons(pageInfo) {
+    const backwardButton = document.getElementById("backward-button");
+    const forwardButton = document.getElementById("forward-button");
+
+    let [currentPage, totalPages] = parsePageInfo(pageInfo);
+
+    if(currentPage === 0)
+        backwardButton.style.display = 'none';
+    else
+        backwardButton.style.display = '';
+
+    if(currentPage+1 >= totalPages)
+        forwardButton.style.display = 'none';
+    else
+        forwardButton.style.display = '';
+}
+
+function parsePageInfo(pageInfo) {
+    if(pageInfo)
+        return [pageInfo.number, pageInfo.totalPages];
+    else
+        return [0, 1];
+}
+
+
+export async function goForward() {
+    pageNumber += 1;
+    await loadTasks()
+}
+
+export async function goBackward() {
+    pageNumber -= 1;
+     await loadTasks()
+}
+
+export async function sortTasksByPriority() {
+    prioritySortOrder = (prioritySortOrder+1) % 3;
+    sortOrders = [["priority", prioritySortOrder], ["finishDate", finishDateSortOrder]];
+    await loadTasks();
+
+    const button = document.getElementById("priority-sort-button");
+    let buttonText = "By priority";
+
+    if(prioritySortOrder === 1)
+        buttonText += " ↓";
+    else if(prioritySortOrder === 2)
+        buttonText += " ↑";
+
+    button.textContent = buttonText;
+}
+
+export async function sortTasksByFinishDate() {
+    finishDateSortOrder = (finishDateSortOrder+1) % 3;
+    sortOrders = [["finishDate", finishDateSortOrder], ["priority", prioritySortOrder]];
+    await loadTasks();
+
+    const button = document.getElementById("finish-date-sort-button");
+    let buttonText = "By finish date";
+
+    if(finishDateSortOrder === 1)
+        buttonText += " ↓";
+    else if(finishDateSortOrder === 2)
+        buttonText += " ↑";
+
+    button.textContent = buttonText;
+}
+
+export async function findTask() {
+    pageNumber = 0;
+    const descriptionField = document.getElementById("descriptionToFind");
+    descriptionToFind = descriptionField.value;
+
+    if(descriptionToFind) {
+        let queryString = `/search/findByDescription?description=${descriptionToFind}`;
+        await loadTasks(queryString);
+        descriptionField.value = '';
+    } else {
+        await loadTasks();
+    }
 }
