880483dd184d1f2306111968fd14e4cffae3adf0
Andrii <andpris527@gmail.com>
2024-08-30 14:47:56 +0300

    Add userDetailsService bean to securityConfig

--- src/main/java/sia/tasks_manager/security/SecurityConfig.java
+++ src/main/java/sia/tasks_manager/security/SecurityConfig.java
@@ -2,8 +2,12 @@ package sia.tasks_manager.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import sia.tasks_manager.data.User;
+import sia.tasks_manager.repositories.UserRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +15,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            User user = userRepository.findByUsername(username);
+            if (user != null) return user;
+
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
     }
 }
