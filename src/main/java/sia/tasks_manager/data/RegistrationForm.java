915b0995d5d53f31734eb5c7427f491d62328f6b
Andrii <andpris527@gmail.com>
2024-08-30 14:53:51 +0300

    Add registrationController

---
+++ src/main/java/sia/tasks_manager/data/RegistrationForm.java
@@ -0,0 +1,9 @@
+package sia.tasks_manager.data;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class RegistrationForm {
+    public User toUser(PasswordEncoder passwordEncoder) {
+        return null;
+    }
+}
