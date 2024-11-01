1f74d680b11a88f2ccfccdb16f7997f8ae99c90a
Andrii <andpris527@gmail.com>
2024-11-01 22:20:41 +0200

    Add subtask

---
+++ src/main/java/sia/tasks_manager/data/Subtask.java
@@ -0,0 +1,37 @@
+package sia.tasks_manager.data;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+public class Subtask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 50, message = "Must be from 1 to 50 characters")
+    private String description;
+
+    @ManyToOne
+    private Task task;
+
+    @NotNull
+    @Min(value = 1, message = "Duration must be 1 day or more")
+    private int duration;
+
+    @ManyToMany
+    @JoinTable(
+            name = "subtask_dependencies",
+            joinColumns = @JoinColumn(name = "subtask_id"),
+            inverseJoinColumns = @JoinColumn(name = "previous_subtask_id")
+    )
+    private Set<Subtask> previousSubtasks = new HashSet<>();
+}
