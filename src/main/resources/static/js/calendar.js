a9d004ff3df140d6379b867c23e2a0334b101916
Andrii <andpris527@gmail.com>
2024-11-14 14:40:37 +0200

    Add simple event display

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,11 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+import {addTaskToDB, loadTasks, goForward, goBackward, sortTasksByFinishDate, sortTasksByPriority, findTask} from 'backend_interaction.js';
+
+document.addEventListener('DOMContentLoaded', async function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
+        initialView: 'dayGridMonth',
+        selectable: true,
+        events: [],
+        eventClick: function(info) {
+            console.log(info.event);
+        }
     });
-
-    calendar.on("dateClick", function (event) {
-        console.log(event);
-    })
     calendar.render();
+
+    window.calendarInstance = calendar;
+
+    await loadTasks();
 });
