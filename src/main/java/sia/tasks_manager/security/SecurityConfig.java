900668c5069cf4a2a6a0dba85671e23ba072cf3f
Andrii <andpris527@gmail.com>
2024-11-18 11:20:05 +0200

    Secure api

--- src/main/java/sia/tasks_manager/security/SecurityConfig.java
+++ src/main/java/sia/tasks_manager/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth.
-                requestMatchers("/tasks/**").
+                requestMatchers("/tasks/**", "/api/**").
                 access(new WebExpressionAuthorizationManager("hasRole('USER')")).
                 requestMatchers("/", "/**").access(new WebExpressionAuthorizationManager("permitAll()"))
         ).formLogin((form) -> form.
