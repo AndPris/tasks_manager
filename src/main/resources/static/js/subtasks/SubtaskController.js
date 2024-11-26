43f9fafda3d0ccd4d888017375fc83947477d653
Andrii <andpris527@gmail.com>
2024-11-26 11:00:51 +0200

    Refactor pagination buttons update

--- src/main/resources/static/js/subtasks/SubtaskController.js
+++ src/main/resources/static/js/subtasks/SubtaskController.js
@@ -1,15 +1,17 @@
 export class SubtaskController {
     subtaskBackendService;
     subtaskDOMService;
+    paginationButtonsDOMService;
 
-    constructor(subtaskBackendService, subtaskDOMService) {
+    constructor(subtaskBackendService, subtaskDOMService, paginationButtonsDOMService) {
         this.subtaskBackendService = subtaskBackendService;
         this.subtaskDOMService = subtaskDOMService;
+        this.paginationButtonsDOMService = paginationButtonsDOMService;
     }
 
     async loadSubtasks() {
         let [subtasks, pageInfo] = await this.subtaskBackendService.loadSubtasks();
-        console.log(subtasks);
         this.subtaskDOMService.displaySubtasks(subtasks);
+        this.paginationButtonsDOMService.updatePaginationButtons(pageInfo);
     }
 }
--- src/main/resources/static/js/subtasks/subtasks_main.js
+++ src/main/resources/static/js/subtasks/subtasks_main.js
@@ -2,6 +2,7 @@
 
 import {SubtaskBackendService} from "SubtaskBackendService.js";
 import {SubtaskDOMService} from "SubtaskDOMService.js";
+import {PaginationButtonsDOMService} from "PaginationButtonsDOMService.js";
 import {SubtaskController} from "SubtaskController.js";
 import {Graph} from "graph.js";
 
@@ -11,7 +12,10 @@ const csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttrib
 const subtaskBackendService = new SubtaskBackendService(taskId, csrfHeader, csrfToken);
 const subtaskDOMService = new SubtaskDOMService(document.getElementById("subtasks-container"),
                                                                 document.getElementById("previous-subtasks"));
-const subtaskController = new SubtaskController(subtaskBackendService, subtaskDOMService);
+const paginationButtonsDOMService = new PaginationButtonsDOMService(document.getElementById("backward-button"),
+                                                                document.getElementById("forward-button"));
+
+const subtaskController = new SubtaskController(subtaskBackendService, subtaskDOMService, paginationButtonsDOMService);
 
 await subtaskController.loadSubtasks();
 
